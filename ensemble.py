ea36432acec3dd510e36c7050cc2233aacf1862c
nopy <g.hejining@gmail.com>
2017-12-16 23:11:02 +0800

    update

--- ensemble.py
+++ ensemble.py
@@ -1,7 +1,14 @@
 import pickle
+import numpy as np
 
 class AdaBoostClassifier:
     '''A simple AdaBoost Classifier.'''
+
+    alpha = None
+    n_weakers_limit = None
+    weak_classifier = None
+    weak_classifier_list = None
+    n_weakers = None
 
     def __init__(self, weak_classifier, n_weakers_limit):
         '''Initialize AdaBoostClassifier
@@ -10,7 +17,8 @@ class AdaBoostClassifier:
             weak_classifier: The class of weak classifier, which is recommend to be sklearn.tree.DecisionTreeClassifier.
             n_weakers_limit: The maximum number of weak classifier the model can use.
         '''
-        pass
+        self.n_weakers_limit = n_weakers_limit
+        self.weak_classifier = weak_classifier
 
     def is_good_enough(self):
         '''Optional'''
@@ -23,9 +31,37 @@ class AdaBoostClassifier:
             X: An ndarray indicating the samples to be trained, which shape should be (n_samples,n_features).
             y: An ndarray indicating the ground-truth labels correspond to X, which shape should be (n_samples,1).
         '''
-        pass
 
+        self.alpha = np.array([])
+        self.n_weakers = 0
+        self.weak_classifier_list = []
+        n_samples = X.shape[0]
+        w = np.ones((n_samples,)) * (1/n_samples)
 
+        y = np.squeeze(y)
+
+        for m in range(self.n_weakers_limit):
+            dtclf = self.weak_classifier(max_depth = 1)
+            dtclf.fit(X,y,sample_weight = w)
+
+            h = dtclf.predict(X)
+            epsilon = np.sum(w[h!=y])
+            print('loop at %d, error rate = %f%%' % (m, epsilon*100))
+            if epsilon>0.5:
+                break
+            alpha = np.log((1-epsilon)/epsilon)/2
+            tmp = w * np.exp(-alpha*y*h)
+            z = np.sum(tmp)
+            w = tmp/z
+            
+            self.alpha = np.append(self.alpha,alpha)
+            self.n_weakers+=1
+            self.weak_classifier_list.append(dtclf)
+
+            AdaBoostClassifier.save(self,'model_%d.pickle'%m)
+
+        # self.alpha = np.asarray(self.alpha).reshape(-1,1)
+        
     def predict_scores(self, X):
         '''Calculate the weighted sum score of the whole base classifiers for given samples.
 
@@ -35,7 +71,17 @@ class AdaBoostClassifier:
         Returns:
             An one-dimension ndarray indicating the scores of differnt samples, which shape should be (n_samples,1).
         '''
-        pass
+        h = []
+        for i in range(self.n_weakers):
+            h.append(self.weak_classifier_list[i].predict(X))
+        # print("n_weak", self.n_weakers)
+        # print("h len",len(h))
+        h = np.asarray(h)
+        # print(self.alpha.shape)
+        # print("h.shape",h.shape)
+        # print("shape", (self.alpha * h).shape)
+        H = np.sum(self.alpha.reshape(-1,1) * h,axis = 0).reshape(-1,1)
+        return H
 
     def predict(self, X, threshold=0):
         '''Predict the catagories for geven samples.
@@ -47,7 +93,10 @@ class AdaBoostClassifier:
         Returns:
             An ndarray consists of predicted labels, which shape should be (n_samples,1).
         '''
-        pass
+        H = self.predict_scores(X)
+        H[H>threshold] = 1
+        H[H<threshold] = -1
+        return H
 
     @staticmethod
     def save(model, filename):
--- train.py
+++ train.py
@@ -1,6 +1,100 @@
+import os
+import random
+import pickle
+from PIL import Image
+import numpy as np
+from feature import NPDFeature
+import multiprocessing
+from sklearn.model_selection import train_test_split
+from ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report, precision_score, recall_score, accuracy_score, f1_score
+
+def load_img_and_get_feature(filename):
+    img = Image.open(filename).convert('L').resize((24, 24), Image.LANCZOS)
+    ft = NPDFeature(np.asarray(img)).extract()
+    return ft
+
+
+def prepareFeature():
+    if os.path.exists('feature.pickle'):
+        print("Found feature pickle file.")
+        return
+    else:
+        print("Feature pickle file not found. Calculating...")
+    
+    basedir = os.path.join('datasets', 'original')
+    
+    with multiprocessing.Pool() as p:
+        X_list = []
+        y_list = []
+        for label in ('nonface', 'face'):
+            imagedir = os.path.join(basedir, label)
+            imagelist = list(map(lambda x: os.path.join(
+                imagedir, x), os.listdir(imagedir)))
+            imagefeature = np.asarray(
+                list(p.map(load_img_and_get_feature, imagelist)))
+            y_t = np.ones((imagefeature.shape[0],1))
+            if label=='nonface':
+                y_t*=-1
+            X_list.append(imagefeature)
+            y_list.append(y_t)
+        X = np.concatenate(X_list)
+        y = np.concatenate(y_list)
+
+        with open('feature.pickle', "wb") as f:
+            pickle.dump((X, y), f)
+
+def preprocessData():
+    if os.path.exists('train_val_data.pickle'):
+        print('Fount train_val_data')
+        with open('train_val_data.pickle','rb') as f:
+            X_train, X_test, y_train, y_test = pickle.load(f)
+    else:
+        print('trian_val_data not found. Generating...')
+
+        with open('feature.pickle', 'rb') as f:
+            X, y = pickle.load(f)
+        
+        idx = np.random.choice(X.shape[0], 1000, replace=False)
+
+        X = X[idx,:]
+        y = y[idx,:]
+
+        X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2)
+        
+        with open('train_val_data.pickle','wb') as f:
+            pickle.dump((X_train, X_test, y_train, y_test),f)
+
+    return X_train, X_test, y_train, y_test
 
 
 if __name__ == "__main__":
-    # write your code here
-    pass
+    prepareFeature()
+    X_train, X_test, y_train, y_test = preprocessData()
 
+    # adaBoost = AdaBoostClassifier(weak_classifier = DecisionTreeClassifier, n_weakers_limit = 100)
+    # adaBoost.fit(X_train,y_train)
+
+    # # AdaBoostClassifier.save(adaBoost, )
+
+    # # adaBoost = AdaBoostClassifier.load('model_1.pickle')
+    # y_predict = adaBoost.predict(X_test)
+    
+    # print("acc", np.sum(y_predict==y_test)/y_predict.shape[0])
+    # print(classification_report(y_test,y_predict, target_names = ('nonface', 'face')))
+
+    for i in range(100):
+        ada = AdaBoostClassifier.load('model_%d.pickle' % i)
+        y_predict_train = ada.predict(X_train)
+        y_predict = ada.predict(X_test)
+        print(i, accuracy_score(y_train,y_predict_train), accuracy_score(y_test,y_predict), '\t',
+        precision_score(y_train,y_predict_train), precision_score(y_test,y_predict),'\t',
+        recall_score(y_train,y_predict_train), recall_score(y_test,y_predict),'\t',
+        f1_score(y_train,y_predict_train), f1_score(y_test,y_predict)
+
+        )
+        # precision_score, recall_score, accuracy_score
+
+        # precision_score, recall_score, accuracy_score, f1_score
+    pass
